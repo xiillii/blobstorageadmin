c3a1bbb7321fa671eb2a75ddb961b3521c170e36
Jose Alonso <mail@josealonso.dev>
2023-07-28 23:15:32 -0600

    Initial backend

---
+++ src/backend/BlobStorageAdmin.Backend.Api/Controllers/ContainerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlobStorageAdmin.Backend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContainerController : ControllerBase
+    {
+    }
+}
